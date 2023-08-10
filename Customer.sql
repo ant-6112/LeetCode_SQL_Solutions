115c95499381101a9e7c56155cc92580c2e0cda8
Antrang Agrawal <antrang6112@gmail.com>
2023-08-10 14:24:07 +0530

    Added SQL Solutions

---
+++ Customer.sql
@@ -0,0 +1,1 @@
+SELECT customer_id, COUNT(visit_id) AS count_no_trans FROM visits INNER JOIN transactions W
---
+++ Replace.sql
@@ -0,0 +1,2 @@
+SELECT unique_id, name FROM EmployeeUNI RIGHT JOIN Employees ON  Employees.id = EmployeeUNI.id;
+
